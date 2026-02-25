cfa59d8c01d6d7b6d5b1c6e2939444db2e0f423f
Lakshay Kapoor <lakshaykapoor08@gmail.com>
2026-02-25 22:04:58 +0530

    SEO improvements.

--- scripts/prerender.tsx
+++ scripts/prerender.tsx
@@ -3,7 +3,7 @@ import path from "node:path";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import { HelmetProvider } from "react-helmet-async";
-import { StaticRouter } from "react-router-dom/server";
+import { StaticRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "../src/AppRoutes";
 
